109e24c890ac5bd8ebcf890064fc000938d20eb7
ysy <2398581361@qq.com>
2024-03-06 09:50:33 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- <div>
+    <NuxtWelcome />
+    <div class="btn text-xl text-green-200">34</div>
+  </div> -->
+  <Index></Index>
+
+</template>
+
+<script setup>
+import Index from "./pages/index";
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
